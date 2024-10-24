8156e2b39e13bbda6dd98654b0de22815eb4f6d9
AswiniGorli <aswinigorli17@gmail.com>
2024-10-24 17:55:49 +0530

    angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CnameComponent } from './cname/cname.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent,
+    CnameComponent
   ],
   imports: [
     BrowserModule,
